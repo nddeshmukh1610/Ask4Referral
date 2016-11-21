e7dbe406d152049adca492aa45d1b43168357164
Nandlal <nddeshmukh16@gmail.com>
2016-11-21 15:15:38 +0530

    ask4referralpart

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,4 +1,3 @@
 class Upload < ActiveRecord::Base
-has_attached_file :image, styles:{medium: "300x300>"thumb: "120x120>" }, default 
-validates_attachment_content_type :image, content_type: /|Aimage|/.*|z/
+
 end
