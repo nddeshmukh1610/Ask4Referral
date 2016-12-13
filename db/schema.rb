646e9748883eed6a37dac7da95a22d8484542444
Ubuntu <ubuntu@ip-172-31-46-171.us-west-2.compute.internal>
2016-12-13 18:54:45 +0000

    layout change

--- db/schema.rb
+++ db/schema.rb
@@ -78,12 +78,6 @@ ActiveRecord::Schema.define(version: 20161126212631) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "japprovals", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "company"
